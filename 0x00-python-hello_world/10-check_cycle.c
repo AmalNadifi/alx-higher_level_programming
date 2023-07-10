047787c9ea480214fb6f379a385261be852d0ae8
AmalNadifi <amal.nadifi9gmail.com>
2023-07-10 12:16:07 -0700

    Task10_updated

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,18 +9,17 @@
 
 int check_cycle(listint_t *list)
 {
-	if (list == NULL) /* if the list is empty, there is no cycle*/
-		return (0);
-	listint_t *ptr1 = list; /* initialize the first pointer
-				   to the head of the list*/
-	listint_t *ptr2 = list; /* initialize the second pointer
-				   to the head of the list*/
+	listint_t *ptr1;
+	listint_t *ptr2;
 
-	while (ptr1 && ptr2->next)
+	if (list == NULL) /* if the list is empty, there is no cycle*/
+                return (0);
+	ptr1 = list; /* initialize 1st pointer to the list head*/
+	ptr2 = list; /* initialize 2nd pointer to list head*/
+	while (ptr1 && ptr2 && ptr2->next)
 	{
 		ptr1 = ptr1->next; /* move the 1st pointer one node at a time*/
-		ptr2 = ptr2->next->next; /*move the 2nd pointer two nodes
-					   at a time*/
+		ptr2 = ptr2->next->next; /*move 2nd pointer 2nodes each time*/
 		if (ptr1 == ptr2) /* if the two pointers meet, there is a cycle*/
 			return (1);
 	}
