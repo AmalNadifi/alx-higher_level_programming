0d08418eb28873471a54173cac9b99cc558c1557
AmalNadifi <amal.nadifi9gmail.com>
2023-07-11 09:51:29 -0700

    Task 14 and 15 updated

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for counter in range(122, 96, -1):
-    if counter % 2 != 0
-    counter = counter - 32
+    if counter % 2 != 0:
+        counter = counter - 32
     print("{}".format(chr(counter)), end="")
