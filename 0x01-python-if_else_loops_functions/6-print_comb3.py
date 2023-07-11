8f70c2d0b2fcf9abedf8c8944436c4d0e3993855
AmalNadifi <amal.nadifi9gmail.com>
2023-07-11 08:28:42 -0700

    Task from 7 to 12 are solved

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for num in range(0, 10):
     for num2 in range(num + 1, 10):
-        print("{}{}".format(num, num2), end='\n' if num == 89 else ", ")
+        print("{}{}".format(num, num2), end='\n' if num == 8 and num2 == 9 \
+else ", ")
