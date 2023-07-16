7f9cb0680d681912bb9cc6b16359e92e5e0ac616
AmalNadifi <amal.nadifi9gmail.com>
2023-07-16 14:43:38 -0700

    pep8_fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         for y in range(len(matrix[x])):
             print("{:d}".format(matrix[x][y]), end="")
             if y != (len(matrix[x]) - 1):
-                print(" ",end="")
+                print(" ", end="")
         print("")
