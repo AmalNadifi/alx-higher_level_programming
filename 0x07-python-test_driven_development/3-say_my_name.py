44fed726b81304cadc862a82d60c0e4dc8d0b630
AmalNadifi <amal.nadifi9@gmail.com>
2023-08-07 12:54:24 -0700

    tasks 2 and 3 solved

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Definition of a module to print first and last name """
+
+def say_my_name(first_name, last_name=""):
+    """ Printing first and last name
+
+    Args:
+        first_name (str): The first name in the name
+        last_name (str, optional): The last name in the name
+
+    Raises:
+        TypeError: if first or last name is not string
+
+    Returns:
+        None
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    
+    full_name = "My name is {} {}".format(first_name, last_name)
+    print(full_name)
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Definition of a module to print a square with '#' character """
+
+def print_square(size):
+    """ The function prints a square using the character '#' based on
+    the given size
+
+    Args:
+        size (int): Size of the square
+
+    Raises:
+        TypeError: if the given size is not integer
+        ValueError: if the given size is < 0
+
+    Returns:
+        None
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    for itr in range(size):
+        print('#' * size)
