bedfca73ba10fc1caab664aba65d8e99d52b4796
AmalNadifi <amal.nadifi9@gmail.com>
2023-08-07 10:20:33 -0700

    add integer file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def add_integers(a, b=98):
+    """ Definition of a function adding 2 integers
+
+    Args:
+        a (int or float): The first number to add
+        b (int or float): The second number to be added
+
+    Returns:
+        int: The resultant sum of 'a' and 'b' after being converted to inegers
+
+    Raises:
+        TypeError: If 'a' or 'b' is not an int or float
+    """
+
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return (a + b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+Readme_0x07-python-test_driven_development
