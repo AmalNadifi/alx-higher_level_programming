1d2855010665e632d71570b32b10fbcf6948fe01
AmalNadifi <amal.nadifi9@gmail.com>
2023-08-09 08:00:50 -0700

    Changed the method names

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,7 +7,7 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """ Test scenarios to evaluate the 'max_integer' function"""
 
-    def t_empty(self):
+    def test_empty(self):
         """ Test with an empty list
 
         Returns:
@@ -15,7 +15,7 @@ class TestMaxInteger(unittest.TestCase):
         """
         self.assertEqual(max_integer(), None)
 
-    def t_pos_int(self):
+    def test_pos_int(self):
         """ Test with a list with positive integers
 
         Returns:
@@ -23,7 +23,7 @@ class TestMaxInteger(unittest.TestCase):
         """
         self.assertEqual(max_integer([18, 1, 19, 100]), 100)
 
-    def t_neg_int(self):
+    def test_neg_int(self):
         """ Test with a list with negative integers
 
         Returns:
@@ -31,7 +31,7 @@ class TestMaxInteger(unittest.TestCase):
         """
         self.assertEqual(max_integer([-18, -1, -19, -100]), -1)
 
-    def t_float(self):
+    def test_float(self):
         """ Test with a list with float numbers
 
         Returns:
@@ -39,7 +39,7 @@ class TestMaxInteger(unittest.TestCase):
         """
         self.assertEqual(max_integer([18.2, 1.8, 19.1, 100]), 100)
 
-    def t_unique(self):
+    def test_unique(self):
         """ Test with a list with a unique number
 
         Returns:
@@ -47,23 +47,23 @@ class TestMaxInteger(unittest.TestCase):
         """
         self.assertEqual(max_integer([100]), 100)
 
-    def t_none(self):
+    def test_none(self):
         """ Test with a list with a parameter 'None' """
         self.assertRaises(TypeError, max_integer, None)
 
-    def t_mixed(self):
+    def test_mixed(self):
         """ Test with a list of mixed types of elements """
         self.assertRaises(TypeError, lambda: max_integer([1, 2.9, "Mixed"]))
 
-    def t_non_iterable_args(self):
+    def test_non_iterable_args(self):
         """ Test with a non iterable args """
         self.assertRaises(TypeError, lambda: max_integer(49))
 
-    def t_multi(self):
+    def test_multi(self):
         """ Test with a multitude of arguments"""
         self.assertRaises(TypeError, lambda: max_integer([19], [18]))
 
-    def t_string(self):
+    def test_string(self):
         """ Test function on The string """
         self.assertEqual(max_integer("Python"), 'y')
 
