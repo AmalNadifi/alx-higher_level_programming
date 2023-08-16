f09fe2ee3ffaf0b89a7970207a4d6e14ca02a30e
AmalNadifi <amal.nadifi9@gmail.com>
2023-08-16 14:58:30 -0700

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Definition of a class Student """
+
+
+class Student:
+    """ Representation of a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initilization of a new Student
+
+        Args:
+            first_name (str): the student first name
+            last_name (str): the student last name
+            age (int): the student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ the public method retrieves a dictionary representation
+        of a Student instance """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
