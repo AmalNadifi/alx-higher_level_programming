608fd3cd209f325a3f346fdb923798d8e7345c76
AmalNadifi <amal.nadifi9@gmail.com>
2023-08-16 14:42:45 -0700

    correctedthe function name

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,7 +2,7 @@
 """ Definition of a function python class to JSON """
 
 
-def class_to_join(obj):
+def class_to_json(obj):
     """ This function returns the the dictionary description with simple
     data structure (list, dictionary, string, integer and boolean) for JSON
     serialization of an object"""
