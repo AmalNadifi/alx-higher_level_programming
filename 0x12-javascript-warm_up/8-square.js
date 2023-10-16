b6844be7267718768c1781b5b350a7efbc8f24d9
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-16 10:18:10 -0700

    8. Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const a = Number(process.argv[2]);
+  for (let ctr = 0; ctr < a; ctr++) {
+    console.log('X'.repeat(a));
+  }
+}
