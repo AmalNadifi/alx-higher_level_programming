39c8b8e03f127ff8b75fa2ebce0a8af207335c9e
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-16 07:58:07 -0700

    2. Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const counter = process.argv.length;
+
+if (counter > 2) {
+	console.log('Argument' + (counter > 3 ? 's' : '') + ' found');
+} else {
+	console.log('No argument');
+}
