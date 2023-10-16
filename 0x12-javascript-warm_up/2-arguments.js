1a12f7e06e7f0c83d1081276b179654f3b13ab84
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-16 08:03:28 -0700

    semistandard fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const counter = process.argv.length;
 
 if (counter > 2) {
-	console.log('Argument' + (counter > 3 ? 's' : '') + ' found');
+  console.log('Argument' + (counter > 3 ? 's' : '') + ' found');
 } else {
-	console.log('No argument');
+  console.log('No argument');
 }
