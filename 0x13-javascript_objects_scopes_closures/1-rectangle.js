fffc97a0cb8a90d505e337394fe565ac4f859e6a
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-17 08:55:50 -0700

    semistandard fixed

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -3,9 +3,9 @@
  * This class represents a rectangle with width and height attributes
  */
 class Rectangle {
-	constructor (w, h) {
-		this.width = w;
-		this.height = h;
-	}
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 module.exports = Rectangle;
