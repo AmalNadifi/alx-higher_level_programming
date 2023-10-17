0968bac515a3048f6538e2d07ce4682147220afa
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-17 09:51:57 -0700

    5. Square #0

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * This class represents a square and inherits from Rectangle
+ * of 4-rectangle.js
+ */
+
+const Rectangle = require('./4-rectangle.js');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
