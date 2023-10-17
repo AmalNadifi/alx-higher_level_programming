bf0fb1f7625e42c8a4dac1ebde7169ac974a9ac0
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-17 11:16:20 -0700

    exports instead of export

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-export.nbOccurences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let counter = 0;
   for (let x = 0; x < list.length; x++) {
     counter = (list[x] === searchElement ? counter + 1 : counter);
