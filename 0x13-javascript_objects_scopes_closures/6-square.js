b9c99a854869ab2e688021cb91c712086bd7853c
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-17 10:58:46 -0700

    semistandard fixed

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,17 +6,17 @@
 
 const PreviousSquare = require('./5-square.js');
 class Square extends PreviousSquare {
-	charPrint (c) {
-		if (c === undefined) {
-			c = 'X';
-		}
-		for (let a = 0; a < this.height; a++) {
-			let myVar = '';
-			for (let b = 0; b < this.width; b++) {
-				myVar += c;
-			}
-			console.log(myVar);
-		}
-	}
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let a = 0; a < this.height; a++) {
+      let myVar = '';
+      for (let b = 0; b < this.width; b++) {
+        myVar += c;
+      }
+      console.log(myVar);
+    }
+  }
 }
 module.exports = Square;
