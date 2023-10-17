8a2d358a2b27ea6b34b952b553a241ced12f764c
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-17 09:28:20 -0700

    3. Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/**
+ * This class represents a rectangle with width and height attributes
+ * Checking the parameters provided
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let a = 0; a < this.height; a++) {
+      let myVar = '';
+      let b = 0;
+      while (b < this.width) {
+        myVar += 'X';
+        b++;
+      }
+      console.log(myVar);
+    }
+  }
+}
+module.exports = Rectangle;
