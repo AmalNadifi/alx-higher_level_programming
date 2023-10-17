77996bcb92f0d879bad0342744c7808ef5ae83ac
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-17 10:52:02 -0700

    6. Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/**
+ * This class represents a square and inherits from Square
+ * of 5-rectangle.js
+ */
+
+const PreviousSquare = require('./5-square.js');
+class Square extends PreviousSquare {
+	charPrint (c) {
+		if (c === undefined) {
+			c = 'X';
+		}
+		for (let a = 0; a < this.height; a++) {
+			let myVar = '';
+			for (let b = 0; b < this.width; b++) {
+				myVar += c;
+			}
+			console.log(myVar);
+		}
+	}
+}
+module.exports = Square;
