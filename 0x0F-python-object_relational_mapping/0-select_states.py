cb0b83434af5b4cf810c243b8e659d7885c5300d
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-19 07:21:31 -0700

    fixed the connection to the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,18 +5,16 @@ the database hbtn_0e_0_usa
 
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == '__main__':
     """
     Accessing the database and getting the states
     """
     # Connecting to the database and retrieving states
-    mysql_username, mysql_password, database_name = sys.argv[1],
-    sys.argv[2], sys.argv[3]
     db = MySQLdb.connect(
-            host="localhost", user=argv[1], passwd=argv[2],
-            db=argv[3], port=3306)
+            host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+            db=sys.argv[3], port=3306)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states")
     rows = cursor.fetchall()
