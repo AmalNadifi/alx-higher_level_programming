6b6bd56fad571d1db1660afea0e79774d22b3afd
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-21 08:08:59 -0700

    fixes and fixes

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -5,6 +5,7 @@ a Base class for utilizing the SQLAlchemy ORM.
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from relationship_state import Base, State
 
 
