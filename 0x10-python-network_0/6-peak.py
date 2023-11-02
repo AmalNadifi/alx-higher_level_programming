377c4a1906b5490dfdada2f2a907414c1d84719c
AmalNadifi <amal.nadifi9@gmail.com>
2023-11-02 08:31:02 -0700

    corrected the pointers names

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """ The following module contains the function that
 finds a peak in a list of unsorted integers """
+
+
 def find_peak(list_of_integers):
     # Checking if the list is empty
-    if not list_of_integers:
+    if not list_of_integers or list_of_integers == []:
         return None
     # Initializing the left and right pointers
     left_ptr, right_ptr = 0, len(list_of_integers) - 1
 
     # Binary search to find the peak
     while left_ptr < right_ptr:
-        mid = (left_ptr + right_ptr) // 2  # Calculating the middle index
+        mid = int((left_ptr + right_ptr) / 2)  # Calculating the middle index
 
         # Comparing the middle element with its right neighbor
         if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1  # Moving left pointer to mid + 1
+            left_ptr = mid + 1  # Moving left pointer to mid + 1
         else:
-            right = mid  # Moving right pointer to mid
+            right_ptr = mid  # Moving right pointer to mid
 
     # Returning the peak element found
-    return list_of_integers[left]
+    return list_of_integers[left_ptr]
