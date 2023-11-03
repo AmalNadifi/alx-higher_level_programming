c81ab68970747de4882e660d6e0ddd3f4f398cbf
AmalNadifi <amal.nadifi9@gmail.com>
2023-11-03 15:11:13 -0700

    solution modified task1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,26 +6,14 @@ import urllib.request
 import sys
 
 
-# Checking if the correct number of command-line arguments is provided
-if len(sys.argv) != 2:
-    sys.stderr.write("Usage: {} <URL>\n".format(sys.argv[0]))
-    sys.exit(1)
+if __name__ == "__main__":
+    # Creating a request object for the specified URL
+    request_obj = urllib.request.Request(sys.argv[1])
 
-# Extracting the URL from the command-line argument
-url = sys.argv[1]
-
-try:
-    # Making a request to the specified URL and handling the response
-    with urllib.request.urlopen(url) as response:
-        # Checking if the 'X-Request-Id' header is present in the response
-        if 'X-Request-Id' in response.headers:
-            x_request_id = response.headers['X-Request-Id']
-            print(x_request_id)
-        else:
-            # Indicating that 'X-Request-Id' was not found in response headers
-            sys.stderr.write("X-Request-Id not found in response headers\n")
-            sys.exit(1)
-except urllib.error.URLError as e:
-    # Handling and displaying errors in case of issues with the request
-    sys.stderr.write("Error while making the request: {}\n".format(e))
-    sys.exit(1)
+    # Sending the request and handling the response
+    with urllib.request.urlopen(request_obj) as response:
+        # Extracting the headers from the response
+        header = dict(resp.headers)
+        
+        # Printing the value of the 'X-Request-Id' header
+        print(header.get("X-Request-Id"))
