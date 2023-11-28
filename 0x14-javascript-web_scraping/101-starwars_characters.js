0d577e9a64dac8ad6d75e47d606a1f5dc3b3e8d1
AmalNadifi <amal.nadifi9@gmail.com>
2023-11-28 12:12:03 -0800

    8. Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const request = require('request');
+const util = require('util');
+
+const filmId = process.argv[2];
+const filmApiUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
+
+const prequest = util.promisify(request);
+
+(async () => {
+  try {
+    const filmData = (await prequest(filmApiUrl, { json: true })).body;
+    
+    // Function to retrieve and log character names
+    const logCharacterName = async (charURL) => {
+      try {
+        const characterData = (await prequest(charURL, { json: true })).body;
+        console.log(characterData.name);
+      } catch (errorChar) {
+        console.error(errorChar);
+      }
+    };
+
+    // Log names of characters asynchronously
+    await Promise.all(filmData.characters.map(logCharacterName));
+  } catch (errorFilm) {
+    console.error(errorFilm);
+  }
+})();
