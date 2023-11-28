5c93b4bb75accc4b7c25e2de34f6e43575214dd8
AmalNadifi <amal.nadifi9@gmail.com>
2023-11-28 08:09:46 -0800

    fixed the comments

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
-// Read the file specified in the command line argument
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
-	// Log either the error or the content to the console
-	// console.log(err || data);
+	console.log(err || data);
 });
